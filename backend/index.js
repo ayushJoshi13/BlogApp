be2228df2f86c35787bc092b6c29412a3597efd2
ayushJoshi13 <AYUSHJOSHI1312@GMAIL.COM>
2025-05-11 20:39:46 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,15 @@ import connectMongoDB from './db/db.js';
 import authRouter from './routes/auth.js'
 import noteRouter from './routes/note.routes.js'
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/note', noteRouter)
 
-app.listen(5000, ()=>{
+app.listen(port || 5001, ()=>{
     connectMongoDB()
     console.log('server is running on port 5000');
     
